d8b737d247f8b5c4bda7b010e4520820b37e02e6
scott susanto <susantoscott@gmail.com>
2024-12-31 17:26:45 -0800

    add server url

--- apps/majordomo/src/lib/env.ts
+++ apps/majordomo/src/lib/env.ts
@@ -4,6 +4,8 @@
  */
 
 export const SERVER_URL =
-  //   process.env.NODE_ENV !== "development"
-  //     ? "https://scottsus.dev"
-  "http://localhost:3000";
+  process.env.NODE_ENV !== "development" || true
+    ? "https://majordomo-web.vercel.app"
+    : "http://localhost:3000";
+
+console.log("server:", SERVER_URL);
